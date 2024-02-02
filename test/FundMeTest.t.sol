fe314982bdd8b0dcf1b03a99962e560e7c925199
anupriyalathey <anupriyalathey@gmail.com>
2024-02-02 23:44:26 +0530

    added basic tests

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -1,20 +1,24 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.18;
+// import "../lib/forge-std/src/Test.sol";
 
-import {Test, console} from "forge-std/Test.sol";
+import {Test, console} from "../lib/forge-std/src/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe; //made a state variable
 
     function setUp() external {
+        // us -> FundMeTest -> fundMe
         fundMe = new FundMe(); // fundMe variable of FundMe type = new FundMe contract
     }
 
     // function testDemo() public {
-        
-    // }
+    //      console.log(number);
+    //     assertEq(number, 2);
+    //  }
+
     function testMinimumDollarIsFive() public {
         assertEq(fundMe.MINIMUM_USD(), 5e18); //test contract gave access to assertEq fn
     }
@@ -25,9 +29,14 @@ contract FundMeTest is Test {
         console.log(msg.sender);
         console.log(address(this));
         // it will show error since 
-        // us -> fundMeTest -> fundMe
-        //so fundMeTest should be owner
-        // assertEq(fundMe.i_owner(), msg.sender);
+        // us -> fundMeTest -> fundMe (us calling FundMeTest which deploys FundMe)
+        // so fundMeTest should be owner of fundMe
+        // assertEq(fundMe.i_owner(), msg.sender); --> test will fail
         assertEq(fundMe.i_owner(), address(this));
     }
+
+    // function testPriceFeedVersionIsAccurate () public {
+    //     uint256 version = fundMe.getVersion();
+    //     assertEq(version, 4);
+    // }
 }
