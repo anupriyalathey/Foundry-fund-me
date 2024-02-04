8ab2a23367cd17de56a5f3024c4cdd02d1f13d22
anupriyalathey <anupriyalathey@gmail.com>
2024-02-05 01:15:49 +0530

    test fund updates

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -10,12 +10,17 @@ import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 contract FundMeTest is Test {
     FundMe fundMe; //made a state variable
 
+    address USER = makeAddr("user");
+    uint256 constant SEND_VALUE = 0.1 ether; // decimals don't work in solidity but 0.1 ether = 10e7
+    uint256 constant STARTING_BALANCE = 10 ether;
+
     function setUp() external {
         // us -> FundMeTest -> fundMe
        // fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306); // fundMe variable of FundMe type = new FundMe contract
        // OR
        DeployFundMe deployFundMe = new DeployFundMe();
        fundMe = deployFundMe.run();
+       vm.deal(USER, STARTING_BALANCE); // Give USER some money
     }
 
     // function testDemo() public {
@@ -42,5 +47,22 @@ contract FundMeTest is Test {
     function testPriceFeedVersionIsAccurate () public {
         uint256 version = fundMe.getVersion();
         assertEq(version, 4); // since we know version should be 4
+    }
+
+    function testFundFailsWithoutEnoughEth() public {
+        vm.expectRevert(); // Hey, the next line should revert!
+        // equivalent to assert(This tx fails/reverts)
+        fundMe.fund(); // send 0 value so this test will pass 
+        
+        // (bool revertAsExpected,) = address(fundMe).call{value: 1e18}("");
+        // assertTrue(revertAsExpected, "expectedRevert: call did not revert");
+    }
+        function testFundUpdatesFundedDataStructure() public {
+            //To be clear about who sends the transaction use pranks(cheatcode from foundry)
+            vm.prank(USER); // The next TX will be sent by USER
+
+            fundMe.fund{value: SEND_VALUE}(); // 10 eth
+            uint256 amountFunded = fundMe.getAddressToAmountFunded(address(USER));
+            assertEq(amountFunded, SEND_VALUE);
     }
 }
